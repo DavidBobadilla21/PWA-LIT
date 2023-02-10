2283244ff98174255a621b6f51c34b68da592423
unknown <david.19070008@itsmotul.edu.mx>
2023-02-09 23:34:47 -0600

    step 6

--- Custom attribute converters/date-converter.js
+++ Custom attribute converters/date-converter.js
@@ -1,11 +1,10 @@
 export const dateConverter = (locale) => {
-    return {
-      toAttribute: (date) => {
-        return date.toLocaleDateString(locale);
-      },
-      fromAttribute: (value) => {
-        return new Date(value);
-      },
-    };
+  return {
+    toAttribute: (date) => {
+      return date.toLocaleDateString(locale);
+    },
+    fromAttribute: (value) => {
+      return new Date(value);
+    },
   };
-  
+};
